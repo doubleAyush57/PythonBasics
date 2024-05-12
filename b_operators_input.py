f19921d5f2d7d668ad46bc308610bd6a691c2782
Aayush <aayush.talreja@gmail.com>
2024-05-12 17:39:49 -0400

    test

--- b_operators_input.py
+++ b_operators_input.py
@@ -1,1 +1,12 @@
-test
+# Operators
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 5)    # decimal division
+print(2 // 5)   # integer division
+print(2 ** 2)   # exponents
+print(5 % 2)    # modulus, remainder operator
+
+# Getting input
+name = input("What is your name?\n")
+print("Your name is", name)
